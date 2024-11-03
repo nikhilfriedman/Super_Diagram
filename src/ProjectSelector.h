3cb39897c21210cd1d51d32bd595c751c1f21034
nikhilfriedman <nikhilooga@gmail.com>
2024-11-03 15:32:50 -0500

    adding project selection funcitonality

---
+++ src/ProjectSelector.h
@@ -0,0 +1,28 @@
+#ifndef PROJECTSELECTOR_H
+#define PROJECTSELECTOR_H
+
+#define IMGUI_DEFINE_MATH_OPERATORS
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+
+#include "imgui_helpers.h"
+
+#include <iostream>
+#include <filesystem>
+
+class ProjectSelector {
+    public:
+        ProjectSelector();
+        ~ProjectSelector();
+
+        
+    private:
+
+};
+
+#endif
